3b20f8f4226c37d24be327d438ce144a6a261ab4
whale22 <realwhale64@naver.com>
2015-11-05 23:25:17 +0900

    CWall의 has..()와 hiyBy()함수 실행을 위한 주석처리
    겹치는 코드 부분을 주석처리
    Line 100부터 108까지

--- proj3_VirtualBilliard/CSphere.cpp
+++ proj3_VirtualBilliard/CSphere.cpp
@@ -97,7 +97,7 @@ void CSphere::ballUpdate(float timeDiff)
 
 		//correction of position of ball
 		// Please uncomment this part because this correction of ball position is necessary when a ball collides with a wall
-		if (tX >= (4.5 - M_RADIUS))
+		/* if (tX >= (4.5 - M_RADIUS))
 			tX = 4.5 - M_RADIUS;
 		else if (tX <= (-4.5 + M_RADIUS))
 			tX = -4.5 + M_RADIUS;
@@ -105,6 +105,7 @@ void CSphere::ballUpdate(float timeDiff)
 			tZ = -3 + M_RADIUS;
 		else if (tZ >= (3 - M_RADIUS))
 			tZ = 3 - M_RADIUS;
+		*/
 
 		this->setCenter(tX, cord.y, tZ);
 	}
