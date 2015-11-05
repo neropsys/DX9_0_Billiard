40892854487eec082465d54b6f05c2b821aadb6a
whale22 <realwhale64@naver.com>
2015-11-05 23:23:55 +0900

    hasIntersected와 hitBy 구현
    함수의 빈 공간 프로그래밍

--- proj3_VirtualBilliard/CWall.cpp
+++ proj3_VirtualBilliard/CWall.cpp
@@ -44,13 +44,58 @@ void CWall::draw(IDirect3DDevice9* pDevice, const D3DXMATRIX& mWorld,
 	pDevice->SetMaterial(&m_mtrl);
 	m_pMesh->DrawSubset(0);
 }
-bool CWall::hasIntersected(CSphere& ball){
-	// Insert your code here.
+
+bool CWall::hasIntersected(CSphere& ball){ 
+
+	// There are two ways to determine intersection
+	// |z - z'| <= r      |      |x - x'| <= r
+	// |z + vzt - z'| <= r   |      |x + vxt - x'| <= r
+	// After Collision
+	// z = z' - r | x = x' - r
+
+	D3DXVECTOR3 cord = ball.getCenter();
+
+	if (cord.x >= (4.5 - M_RADIUS))  return true;
+	//cord.x = 4.5 - M_RADIUS;
+	if (cord.x <= (-4.5 + M_RADIUS)) return true;
+	//cord.x = -4.5 + M_RADIUS;
+	if (cord.z <= (-3 + M_RADIUS))    return true;
+	//cord.z = -3 + M_RADIUS;
+	if (cord.z >= (3 - M_RADIUS))    return true;
+	//cord.z = 3 - M_RADIUS;
+
 	return false;
 }
-void CWall::hitBy(CSphere& ball){
-	// Insert your code here.
+
+void CWall::hitBy(CSphere& ball){ // 부딪혔을 경우(hasIntersected == true) 상단/하단 벽의 경우 z방향만 1/2와 반대로, 좌단/우단의 경우 x방향만 1/2와 반대로
+
+	D3DXVECTOR3 cord = ball.getCenter();
+	if (hasIntersected(ball)){
+
+		if (cord.z >= (3 - M_RADIUS))  // 공의 Z값이 아래로 치우친 경우 ( 하측 벽에 부딪힌 경우 )
+		{
+			ball.setCenter(cord.x, cord.y, 3 - M_RADIUS); // 벽에 붙인 후
+			ball.setPower(ball.getVelocity_X(), ball.getVelocity_Z()*(0.5)*(-1)); // 속도 반감
+		}
+		if (cord.z <= (-3 + M_RADIUS)) // 공의 Z값이 위로 치우친 경우 ( 상측 벽에 부딪힌 경우 )
+		{
+			ball.setCenter(cord.x, cord.y, -3 + M_RADIUS); // 벽에 붙인 후
+			ball.setPower(ball.getVelocity_X(), ball.getVelocity_Z()*(0.5)*(-1)); // 속도 반감
+		}
+		if (cord.x >= (4.5 - M_RADIUS)) // 공의 X값이 우측으로 치우친 경우 ( 우측 벽에 부딪힌 경우 )
+		{
+			ball.setCenter(4.5 - M_RADIUS,cord.y,cord.z); // 벽에 붙인 후
+			ball.setPower(ball.getVelocity_X()*(0.5)*(-1), ball.getVelocity_Z()); // 속도 반감
+		}
+
+		if (cord.x <= (-4.5 + M_RADIUS)) // 공의 X값이 좌측으로 치우친 경우 ( 좌측 벽에 부딪힌 경우 )
+		{
+			ball.setCenter(-4.5 + M_RADIUS, cord.y, cord.z); // 벽에 붙인 후
+			ball.setPower(ball.getVelocity_X()*(0.5)*(-1), ball.getVelocity_Z()); // 속도 반감
+		}
+	}
 }
+
 
 void CWall::setPosition(float x, float y, float z)
 {
